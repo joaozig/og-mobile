c8fb37b9bc14e9cf895a58b9b7aeb138544ad17a
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-30 21:01:19 -0300

    Adds #getSport and refactors the MainService

--- www/components/main/main.service.js
+++ www/components/main/main.service.js
@@ -2,18 +2,46 @@ angular.module('main')
 
 .service('MainService', function($q) {
 
-	this.getSports = function() {
+	var service = this;
+
+	/* Properties */
+	service.sports = [
+		{id: 1, name: 'Futebol Americano'},
+		{id: 2, name: 'Basquete'},
+		{id: 3, name: 'Futebol'},
+		{id: 4, name: 'Volei'}
+	];
+
+	/* Methods */
+	service.getSport = getSport;
+	service.getSports = getSports;
+
+	/**********/
+
+	function getSport(sportId) {
+		var deferred = $q.defer();
+		var sport = {};
+
+		sport = service.sports.find(function (sport) {
+		    return sport.id === parseInt(sportId);
+		});
+
+    if(sport) {
+    	console.log(sport)
+    	deferred.resolve(sport)
+    } else {
+      deferred.reject('Esporte não encontrado.')
+    }
+
+	  return deferred.promise;
+	}
+
+	function getSports() {
 		var deferred = $q.defer();
 		var successfulHttpRequest = true;
 
     if(successfulHttpRequest) {
-			var sports = [
-				{id: 1, name: 'Futebol Americano'},
-				{id: 2, name: 'Basquete'},
-				{id: 3, name: 'Futebol'},
-				{id: 4, name: 'Volei'}
-			];
-    	deferred.resolve(sports)
+    	deferred.resolve(service.sports)
     } else {
       deferred.reject('Não foi possível recuperar os esportes.')
     }
