638e3f2d8cc3a310f183ac3bc666f14776960b72
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-01 18:36:18 -0300

    Adds Contries Service

---
+++ www/components/countries/countries.service.js
@@ -0,0 +1,51 @@
+angular.module('countries')
+
+.service('CountriesService', function($q) {
+
+	var service = this;
+
+	/* Properties */
+	service.countries = [
+		{id: 1, name: 'Argentina'},
+		{id: 2, name: 'Brasil'},
+		{id: 3, name: 'Estados Unidos'},
+		{id: 4, name: 'França'}
+		{id: 5, name: 'Portugal'}
+	];
+
+	/* Methods */
+	service.getCountry = getCountry;
+	service.getContries = getCountry;
+
+	/**********/
+
+	function getCountry(countryId) {
+		var deferred = $q.defer();
+		var country = {};
+
+		country = service.countries.find(function (country) {
+		    return country.id === parseInt(countryId);
+		});
+
+    if(country) {
+    	deferred.resolve(country)
+    } else {
+      deferred.reject('País não encontrado.')
+    }
+
+	  return deferred.promise;
+	}
+
+	function getCountries() {
+		var deferred = $q.defer();
+		var successfulHttpRequest = true;
+
+    if(successfulHttpRequest) {
+    	deferred.resolve(service.countries)
+    } else {
+      deferred.reject('Não foi possível recuperar os países.')
+    }
+
+	  return deferred.promise;
+	}
+});
