d375db5c8747fcdc15e20f2e89582ec14fe64986
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-02 15:02:18 -0300

    Adds BetService

---
+++ tests/unit-tests/components/bet/bet.service.tests.js
@@ -0,0 +1,125 @@
+describe('BetService', function() {
+
+	var service;
+	var BET_CONSTANT = 'bet';
+	var $rootScope;
+	var betMock = {
+		playerName: 'Player Name',
+		betAmount: 100.50,
+		bets: 2
+	};
+
+	beforeEach(module('bet'));
+
+	beforeEach(inject(function($injector, _$rootScope_) {
+		$rootScope = _$rootScope_;
+		service = $injector.get('BetService');
+	}));
+
+  describe('#addBet', function() {
+  	var playerName, betAmount, expectedBet;
+
+  	beforeEach(function() {
+			playerName = 'New Player';
+			betAmount = 50;
+			expectedBet = {
+				playerName: playerName,
+				betAmount: betAmount,
+				bets: 0
+			};
+  	});
+
+  	describe('when it successful', function() {
+  		it('should return the new bet object', function() {
+				service.addBet(playerName, betAmount).then(
+					function(bet) {
+						expect(expectedBet).toEqual(bet);
+					},
+					function(errorMessage) {
+						expect(errorMessage).toEqual(null);
+					}
+				);
+				$rootScope.$apply();
+  		});
+  	});
+
+  	describe('when it unsuccessful', function() {
+  		beforeEach(function() {
+				window.localStorage.removeItem(BET_CONSTANT);
+  		});
+
+  		describe('because the playerName is blank', function() {
+	  		it('should not set a new bet object', function() {
+					playerName = '';
+					service.addBet(playerName, betAmount).then(
+						function(bet) {
+							expect(bet).toEqual(null);
+						},
+						function(errorMessage) {
+							expect(errorMessage).toEqual('Preencha o nome do apostador');
+						}
+					);
+					$rootScope.$apply();
+	  		});
+  		});
+  		describe('because the betAmount is blank', function() {
+	  		it('should not set a new bet object', function() {
+					betAmount = '';
+					service.addBet(playerName, betAmount).then(
+						function(bet) {
+							expect(bet).toEqual(null);
+						},
+						function(errorMessage) {
+							expect(errorMessage).toEqual('Preencha o valor a ser apostado');
+						}
+					);
+					$rootScope.$apply();
+	  		});
+  		});
+  		describe('because the betAmount is negative', function() {
+	  		it('should not set a new bet object', function() {
+					betAmount = '-2';
+					service.addBet(playerName, betAmount).then(
+						function(bet) {
+							expect(bet).toEqual(null);
+						},
+						function(errorMessage) {
+							expect(errorMessage).toEqual('O valor a ser apostado tem que ser maior que 0');
+						}
+					);
+					$rootScope.$apply();
+	  		});
+  		});
+  		describe('because the betAmount is zero', function() {
+	  		it('should not set a new bet object', function() {
+					betAmount = '0';
+					service.addBet(playerName, betAmount).then(
+						function(bet) {
+							expect(bet).toEqual(null);
+						},
+						function(errorMessage) {
+							expect(errorMessage).toEqual('O valor a ser apostado tem que ser maior que 0');
+						}
+					);
+					$rootScope.$apply();
+	  		});
+  		});
+  	});
+  });
+
+  describe('#getBet', function() {
+  	describe('when there is an active bet', function() {
+  		it('should return the bet object', function() {
+				window.localStorage.setItem(BET_CONSTANT, JSON.stringify(betMock));
+				var betReturned = service.getBet();
+				expect(betReturned).toEqual(betMock);
+  		});
+  	});
+  	describe('when there is no bet', function() {
+			it('should return null', function() {
+				window.localStorage.removeItem(BET_CONSTANT);
+				expect(service.getBet()).toEqual(null);
+			});
+  	});
+  });
+});
---
+++ www/components/bet/bet.service.js
@@ -0,0 +1,63 @@
+angular.module('bet')
+.constant('BET', 'bet')
+.service('BetService', function(BET, $q) {
+
+	var service = this;
+
+	/* Properties */
+
+	/* Public Methods */
+	service.addBet = addBet;
+	service.getBet = getBet;
+
+	/* Initialization */
+	init();
+
+	/**** Methods definition ****/
+	function init() {
+
+	}
+
+	function addBet(playerName, betAmount) {
+		var deferred = $q.defer();
+
+		var validation = validateBet(playerName, betAmount);
+		if(validation.valid) {
+			var bet = {
+				playerName: playerName,
+				betAmount: betAmount,
+				bets: 0
+			};
+
+			window.localStorage.setItem(BET, JSON.stringify(bet));
+			deferred.resolve(bet);
+		} else {
+			deferred.reject(validation.message);
+		}
+
+		return deferred.promise;
+	}
+
+	function getBet() {
+		return JSON.parse(window.localStorage.getItem(BET));
+	}
+
+	/* Private Methods */
+
+	function validateBet(playerName, betAmount) {
+		betAmount = parseFloat(betAmount);
+		var validation = {valid: false, message: 'Falha ao adicionar aposta'};
+		if(!playerName) {
+			validation.message = 'Preencha o nome do apostador';
+		} else if(isNaN(betAmount)) {
+			validation.message = 'Preencha o valor a ser apostado';
+		} else if(betAmount <= 0) {
+			validation.message = 'O valor a ser apostado tem que ser maior que 0';
+		} else {
+			validation.valid = true;
+			validation.message = 'Aposta válida';
+		}
+
+		return validation;
+	}
+});
