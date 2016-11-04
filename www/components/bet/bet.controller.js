dc375d0bf1ad93e94b311bed7956003f1112bf9c
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-04 19:19:13 -0300

    Bet Detail template

--- www/components/bet/bet.controller.js
+++ www/components/bet/bet.controller.js
@@ -1,5 +1,17 @@
 angular.module('bet')
 
-.controller('BetController', function() {
+.controller('BetController', function(BetService) {
+	var vm = this;
+	vm.util = new Util();
 
+	/* Properties */
+	vm.bet = null;
+
+	/* Initialization */
+	init();
+
+	/*********/
+	function init() {
+		vm.bet = BetService.getBet();
+	}
 });
