027048b7844e37269854cf50584e1b90d17f39f3
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-07 22:16:39 -0300

    Implements the remove ticket from bet at BetView

---
+++ tests/unit-tests/components/bet/bet.controller.tests.js
@@ -0,0 +1,84 @@
+describe('BetController', function() {
+
+	var controller;
+	var betServiceMock;
+	var ionicPopupMock;
+	var deferredConfirm;
+	var $rootScope;
+	var betMock = new Bet({
+		playerName: 'Player',
+		betAmount: 100,
+		tickets: []
+	});
+
+	beforeEach(module('bet'));
+
+	beforeEach(inject(function(_$rootScope_, $controller, $q) {
+		deferredConfirm = $q.defer();
+		$rootScope = _$rootScope_;
+
+		// mock BetService
+		betServiceMock = {
+			getBet: jasmine.createSpy('getBet spy').and.returnValue(betMock),
+			removeTicket: jasmine.createSpy('removeTicket spy').and.returnValue(true)
+		};
+    // mock $ionicPopup
+    ionicPopupMock = {
+    	alert: jasmine.createSpy('alert spy'),
+    	confirm: jasmine.createSpy('confirm spy').and.returnValue(deferredConfirm.promise)
+    }
+
+		controller = $controller('BetController', {
+			$ionicPopup: ionicPopupMock,
+			BetService: betServiceMock
+		});
+	}));
+
+  describe('Initialization', function() {
+		it('should have a bet object from BetService', function() {
+			expect(controller.bet).toEqual(betMock);
+		});
+
+		describe('when the init is executed', function() {
+			describe('at BetService#getBet', function() {
+		  	it('if there is a bet, should return the bet object', function() {
+		  		expect(betServiceMock.getBet).toHaveBeenCalled();
+		  	});
+			});
+		});
+  });
+
+  describe('#removeTicket', function() {
+  	beforeEach(function() {
+  		controller.removeTicket(1);
+  	});
+
+  	it('should open a confirm popup', function() {
+  		$rootScope.$digest();
+  		expect(ionicPopupMock.confirm).toHaveBeenCalledWith({
+				title: 'Remover Palpite',
+				template: 'Você deseja realmente remover o palpite da aposta?'
+			});
+  	});
+
+  	describe('when it successful', function() {
+  		it('should reset local bet', function() {
+	  		deferredConfirm.resolve(true);
+	  		$rootScope.$digest();
+	  		expect(betServiceMock.getBet).toHaveBeenCalled();
+  		});
+  	});
+  	describe('when it unsuccessful', function() {
+  		it('should show a popup with error message', function() {
+				// mock BetService
+				betServiceMock.removeTicket = jasmine.createSpy('removeTicket spy').and.returnValue(false);
+	  		deferredConfirm.resolve(true);
+	  		$rootScope.$digest();
+	  		expect(ionicPopupMock.alert).toHaveBeenCalledWith({
+					title: 'Algo falhou :(',
+					template: 'Não foi possível remover o palpite da aposta'
+				});
+  		});
+  	});
+  });
+});
--- www/components/bet/bet.controller.js
+++ www/components/bet/bet.controller.js
@@ -1,17 +1,46 @@
 angular.module('bet')
 
-.controller('BetController', function(BetService) {
+.controller('BetController', function($ionicPopup, BetService) {
 	var vm = this;
 	vm.util = new Util();
 
 	/* Properties */
 	vm.bet = null;
 
+	/* Public Methods */
+	vm.removeTicket = removeTicket;
+
 	/* Initialization */
 	init();
 
 	/*********/
 	function init() {
+		_setBet();
+	}
+
+	function removeTicket(ticketId) {
+		var confirmPopup = $ionicPopup.confirm({
+			title: 'Remover Palpite',
+			template: 'Você deseja realmente remover o palpite da aposta?'
+		});
+
+		confirmPopup.then(function(confirmed) {
+			if(confirmed) {
+				if(BetService.removeTicket(ticketId)) {
+					_setBet();
+				} else {
+					$ionicPopup.alert({
+						title: 'Algo falhou :(',
+						template: 'Não foi possível remover o palpite da aposta'
+					});
+				}
+			}
+		});
+	}
+
+	/* Private Methods */
+
+	function _setBet() {
 		vm.bet = BetService.getBet();
 	}
 });
